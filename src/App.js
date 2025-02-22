1990688f60920155d36b065cd1f3dd0b9f7469d7
Mayankyadav3980 <ymayank501@gmail.com>
2025-02-22 19:39:04 +0530

    fetching and updating data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,96 @@
-import React, { useState } from 'react'
-import './App.css'
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
-    const [current, setCurrent] = useState(0);
-    let lenght = imgArr.length;
+  console.log("inside app compo at top");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(null);
+  const [reset, setReset] = useState(false);
 
-    const prevImg= ()=>{
-      setCurrent(current == 0? lenght-1 : current-1);
-      console.log(current);
-      
-    }
-
-    const nextImg = () => {
-      setCurrent(current == lenght-1 ? 0 : current+1);
-      console.log(current);
-    };
+  const getData = async ()=>{
     
+    try{
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      if(res.ok){
+        const userData = await res.json();
+        setData(userData.map(obj=>({...obj, count: 0})));
+      }
+    }catch(e){
+      setErr(e);
+    }finally{
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=>{
+    console.log("inside useEffect");
+    getData();
+
+    return ()=> console.log("compo is unmounting")
+  },[reset]);
+
+  const handleDec = (id)=>{
+    setData(pv=>{
+     return pv.map(obj=>{
+        if(obj.id === id){
+          return {...obj, count: obj.count-1};
+        }
+        return obj;
+      })
+    })
+  }
+  const handleInc = (id)=>{
+    setData(pv=>{
+     return pv.map(obj=>{
+        if(obj.id === id){
+          return {...obj, count: obj.count+1};
+        }
+        return obj;
+      })
+    })
+  }
+
+  
+
+  console.log("below useEffect");
+  if(loading) return <h1>Loading...</h1>
+  if(err) return <h1>Something went wrong!!</h1>
+
   return (
-    <div className="carousel_container">
-      <FaArrowAltCircleLeft className="left-arrow arrow" onClick={prevImg} />
-      <FaArrowAltCircleRight className="right-arrow arrow" onClick={nextImg} />
-      <div className="modal">
-        {imgArr.map((url, idx) => {
-          if(current == idx)
-          return <img key={idx} className="carousel_image" src={url} alt="img" />;
-        })}
-      </div>
-    </div>
+    <>
+      <table>
+        <caption>User Database</caption>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th colSpan="2">Income</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((obj) => {
+            return (
+              <tr key={obj.id}>
+                <td>{obj.id}</td>
+                <td>{obj.name}</td>
+                <td>{obj.email}</td>
+                <td>
+                  {" "}
+                  <button onClick={() => handleDec(obj.id)}>-</button>
+                  {obj.count}
+                  <button onClick={() => handleInc(obj.id)}>+</button>
+                </td>
+                <td>?</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={()=>setReset(pv=>!pv)}>Reset Data</button>
+    </>
   );
-}
+};
 
-export default App
-
-
-const imgArr = [
-  "https://c8.alamy.com/comp/PT3D01/jagannath-temple-at-puri-odisha-india-PT3D01.jpg",
-  "https://i.pinimg.com/originals/69/df/42/69df426b5d499acc756cffc982b8e432.jpg"
-];
+export default App;
