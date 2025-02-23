fe3a8f2bd532334c6b7e912e2e26a3b36a0113c1
Mayankyadav3980 <ymayank501@gmail.com>
2025-02-23 12:35:15 +0530

    CRUD done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  console.log("inside app compo at top");
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState(null);
+  const [ud, setUd] = useState({
+    id: "",
+    name: "",
+    email: ""
+  });
+  const [edit, setEdit] = useState(false);
+
   const [reset, setReset] = useState(false);
 
   const getData = async ()=>{
@@ -24,41 +30,86 @@ const App = () => {
   }
 
   useEffect(()=>{
-    console.log("inside useEffect");
     getData();
-
-    return ()=> console.log("compo is unmounting")
   },[reset]);
 
   const handleDec = (id)=>{
-    setData(pv=>{
-     return pv.map(obj=>{
-        if(obj.id === id){
-          return {...obj, count: obj.count-1};
-        }
-        return obj;
-      })
-    })
+    setData((pv) => pv.map((obj) => (obj.id === id ? { ...obj, count: obj.count - 1 } : obj))
+    );
   }
   const handleInc = (id)=>{
-    setData(pv=>{
-     return pv.map(obj=>{
-        if(obj.id === id){
-          return {...obj, count: obj.count+1};
-        }
-        return obj;
-      })
-    })
+    setData(pv=> pv.map(obj=>(obj.id === id) ? {...obj, count: obj.count+1} : obj))
   }
 
-  
+  const handleDelete = (id) => setData((prevState) => prevState.filter(obj=> obj.id !== id))
 
-  console.log("below useEffect");
+ 
+  
+  const handleInput = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+
+    setUd(ps => ({...ps, [field]: value}))
+  }
+  
+   const handleEdit = (id) => {
+     setEdit(!edit);
+     let user = data.find((obj) => obj.id === id);
+     setUd(user);
+   };
+
+  const handleUpdate = (e) => {
+    setData(ps => ps.map(obj => obj.id === ud.id ? ud : obj))
+     setUd({
+       id: "",
+       name: "",
+       email: "",
+     });
+     setEdit(false)
+     e.preventDefault();
+  }
+
+  const handleSubmit = (e) => {
+   
+    setData(ps => ([...ps, ud]))
+    // data.push(ud);
+    setUd({
+      id: "",
+      name: "",
+      email: "",
+    });
+     e.preventDefault();
+  }
+
   if(loading) return <h1>Loading...</h1>
   if(err) return <h1>Something went wrong!!</h1>
 
   return (
     <>
+      <form onSubmit={ edit ? handleUpdate : handleSubmit }>
+        <input
+          placeholder="id"
+          required
+          name="id"
+          value={ud.id}
+          onChange={handleInput}
+        />
+        <input
+          placeholder="name"
+          required
+          name="name"
+          value={ud.name}
+          onChange={handleInput}
+        />
+        <input
+          placeholder="email"
+          required
+          name="email"
+          value={ud.email}
+          onChange={handleInput}
+        />
+        <button type="submit">{edit ? "Update" : "Add"}</button>
+      </form>
       <table>
         <caption>User Database</caption>
         <thead>
@@ -66,7 +117,8 @@ const App = () => {
             <th>Id</th>
             <th>Name</th>
             <th>Email</th>
-            <th colSpan="2">Income</th>
+            <th>Income</th>
+            <th colSpan="2">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -82,13 +134,18 @@ const App = () => {
                   {obj.count}
                   <button onClick={() => handleInc(obj.id)}>+</button>
                 </td>
-                <td>?</td>
+                <td>
+                  <button onClick={() => handleDelete(obj.id)}>Delete</button>
+                </td>
+                <td>
+                  <button onClick={() => handleEdit(obj.id)}>Update</button>
+                </td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <button onClick={()=>setReset(pv=>!pv)}>Reset Data</button>
+      <button onClick={() => setReset((pv) => !pv)}>Reset Data</button>
     </>
   );
 };
