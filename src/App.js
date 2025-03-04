8d075ba4641a73375ab8e049e59bc2556805415c
Mayankyadav3980 <ymayank501@gmail.com>
2025-03-04 09:09:33 +0530

    subtle update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Us from "./components/Us";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-
-  const router = createBrowserRouter([
-    {
-      path:"/", element: <Navbar/>, children:[
-        {index:true, element: <Home/>},
-        {path:"/about", element: <About/>},
-        {path:"/us", element: <Us/>},
-      ]
-    }
-  ])
   return (
-    <RouterProvider router={router}/>
+    <div>App</div>
   )
-}
+};
 
-export default App
+export default App;
