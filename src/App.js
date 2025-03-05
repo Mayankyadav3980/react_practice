04278a9dda70396bf4d4b429f0172c2038b38b63
Mayankyadav3980 <ymayank501@gmail.com>
2025-03-05 19:29:26 +0530

    useMemo implemented

--- src/App.js
+++ src/App.js
@@ -1,84 +1,31 @@
-import React, { useState, useEffect, useRef } from "react";
-import './App.css'
+import React, { useEffect, useMemo, useState } from "react";
+import { useDarkMode } from "./components/CustomHook";
+import "./App.css";
+
+const nums = new Array(5).fill(0).map((_, ind) => {
+
+    return {
+      id: ind,
+      token: ind === 4 ? "hello moto" : "buy moto(not4)"
+    };
+  
+});
+
+
 const App = () => {
-  const [data, setData] = useState([]);
-  const fieldRef = useRef();
-  const [field, setField] = useState('name');
-  const [suggList, setSuggList] = useState([]);
-  const [inp, setInput] = useState('');
+  // const [theme, ToggleTheme] = useDarkMode(localStorage.getItem("theme"));
+  const [count, setcount] = useState(0)
+  const [numbers, setNumbers] = useState(nums);
 
-  const getData = async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      if (!res.ok) {
-        console.log("res not ok");
-        return;
-      }
-      const userData = await res.json();
-      setData(userData);
-    } catch (err) {
-      console.log("err occured");
-    }
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getSuggestionList = (namee) => {
-
-    console.log('field: ',field);
-    console.log('searched term: ',namee);
-    const list = data.filter((obj) => {
-      return obj[field].toLowerCase().includes(namee.toLowerCase());
-    });
-    console.log('slist is:', list)
-    setSuggList(list);
-  };
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setInput(value);
-    getSuggestionList(value);
-  };
-
-  const handleFieldChange = () => {
-    setField(fieldRef.current.value);
-    setInput('');
-    setSuggList([]);
-  }
+  // const ourValue = nums.find(obj => obj.id===4);  //expansive computation
+  const memoUseValue = useMemo(()=>{ console.log('memo rendered'); return nums.find(obj => obj.id===4);}, [] )
 
   return (
-    <div>
-      <input placeholder="search here" value={inp} onChange={handleChange} />
-      <div>
-        {suggList.length > 0 && (
-          <ul>
-            {suggList.map((obj) => (
-              <li
-                key={obj.id}
-                onClick={() => {setInput(obj[field]); setSuggList([]) } }
-              >
-                {obj[field]}
-              </li>
-            ))}
-          </ul>
-        ) }
-      </div>
-
-      <h2>dropdown menu</h2>
-      <label htmlFor="field">Choose a field:</label>
-      {data.length > 0 && (
-        <select name="field" id="fields" ref={fieldRef} onChange={handleFieldChange}>
-          {Object.keys(data[0]).map((f, idx) =>  (
-              <option key={idx} value={f}>
-                {f}
-              </option>
-            )
-          )}
-        </select>
-      )}
-    </div>
+    <>
+      <div>{count}</div>
+      <button onClick={() => setcount(count + 1)}>inc count</button>
+      <div>{memoUseValue.token}</div>
+    </>
   );
 };
 
